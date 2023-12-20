fbffb6e10d026e2475d0eb6132b9238dbb9901f3
JeromeM92 <jerome.marras@hotmail.fr>
2023-12-20 21:04:20 +0100

    change environment variable

--- src/main/java/com/example/apimercadona/ws/ProductWs.java
+++ src/main/java/com/example/apimercadona/ws/ProductWs.java
@@ -1,19 +1,61 @@
 package com.example.apimercadona.ws;
 
+import com.example.apimercadona.entity.Category;
+import com.example.apimercadona.entity.Deal;
 import com.example.apimercadona.entity.Product;
+import com.example.apimercadona.repository.CategoryRepository;
+import com.example.apimercadona.repository.DealRepository;
 import com.example.apimercadona.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RequestMapping(ApiRegistration.API_REST + ApiRegistration.PRODUCT)
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProductWs {
+   @Autowired
+   private ProductService productService;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Autowired
-    private ProductService productService;
+    private DealRepository dealRepository;
+    private Category getCategory(Long categoryId) {
+        if (categoryId != null) {
+            return categoryRepository.findById(categoryId).orElse(null);
+        }
+        return null;
+    }
+
+    private Deal getDeal(Long dealId) {
+        if (dealId != null) {
+            return dealRepository.findById(dealId).orElse(null);
+        }
+        return null;
+    }
+    private String storeImage(MultipartFile file) {
+        if (file.isEmpty()) {
+            return null;
+        }
+
+        try {
+            String tempDir = System.getProperty("java.io.tmpdir");
+            Path destinationFile = Paths.get(tempDir, file.getOriginalFilename());
+            Files.copy(file.getInputStream(), destinationFile);
+
+            return destinationFile.toString();
+        } catch (Exception e) {
+            // Gérer l'exception
+            return null;
+        }
+    }
 
     @GetMapping("/all-products")
     public List<Product> getAllProducts() {
@@ -27,7 +69,23 @@ public class ProductWs {
     }
 
     @PostMapping("/create-product")
-    public void createProduct(@RequestBody Product product) {
+    public void createProduct(@RequestParam("productName") String productName,
+                              @RequestParam("price") Float price,
+                              @RequestParam("description") String description,
+                              @RequestParam("category") Long categoryId,
+                              @RequestParam(value = "deal", required = false) Long dealId,
+                              @RequestParam("image") MultipartFile imageFile) {
+        String imageUrl = storeImage(imageFile);
+        Product product = new Product();
+        product.setProductName(productName);
+        product.setPrice(price);
+        product.setDescription(description);
+        Category category = getCategory(categoryId);
+        Deal deal = getDeal(dealId);
+        product.setCategory(category);
+        product.setDeal(deal);
+        product.setImageUrl(imageUrl);
+
         productService.createProduct(product);
     }
 
