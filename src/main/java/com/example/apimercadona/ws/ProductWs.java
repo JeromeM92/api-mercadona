71d2833e7f50389d3b9dc4e442af6d5714581731
JeromeM92 <jerome.marras@hotmail.fr>
2023-12-20 21:58:56 +0100

    Add ProductDto, modify type image

--- src/main/java/com/example/apimercadona/ws/ProductWs.java
+++ src/main/java/com/example/apimercadona/ws/ProductWs.java
@@ -3,10 +3,13 @@ package com.example.apimercadona.ws;
 import com.example.apimercadona.entity.Category;
 import com.example.apimercadona.entity.Deal;
 import com.example.apimercadona.entity.Product;
+import com.example.apimercadona.payload.ProductDto;
 import com.example.apimercadona.repository.CategoryRepository;
 import com.example.apimercadona.repository.DealRepository;
 import com.example.apimercadona.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,6 +17,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.List;
 
 @RequestMapping(ApiRegistration.API_REST + ApiRegistration.PRODUCT)
@@ -63,12 +67,37 @@ public class ProductWs {
         return productService.getAllProducts();
     }
 
+    @PostMapping("/create-product")
+    public ResponseEntity<?> createProduct(@RequestBody ProductDto productDto) {
+        try {
+            byte[] imageBytes = Base64.getDecoder().decode(productDto.getImageBase64());
+
+            Product product = new Product();
+            // Settez les autres champs du produit
+            product.setImage(imageBytes); // Stockez les données binaires de l'image
+            product.setProductName(productDto.getProductName());
+            product.setPrice(productDto.getPrice());
+            product.setDescription(productDto.getDescription());
+            Category category = getCategory(productDto.getCategoryId());
+            Deal deal = getDeal(productDto.getDealId());
+
+            product.setCategory(category);
+            product.setDeal(deal);
+            productService.createProduct(product);
+
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            // Gérer l'exception
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
     @GetMapping("{productId}")
     public Product getProductById(@PathVariable("productId") Long productId) {
         return null;
     }
 
-    @PostMapping("/create-product")
+    /*@PostMapping("/create-product")
     public void createProduct(@RequestParam("productName") String productName,
                               @RequestParam("price") Float price,
                               @RequestParam("description") String description,
@@ -87,7 +116,7 @@ public class ProductWs {
         product.setImageUrl(imageUrl);
 
         productService.createProduct(product);
-    }
+    }*/
 
     @PutMapping("/update")
     public void updateProductById(@RequestBody Product updatedProduct, @PathVariable Long productId) {
@@ -96,7 +125,7 @@ public class ProductWs {
         existingProduct.setProductName(updatedProduct.getProductName());
         existingProduct.setPrice(updatedProduct.getPrice());
         existingProduct.setDescription(updatedProduct.getDescription());
-        existingProduct.setImageUrl(updatedProduct.getImageUrl());
+        existingProduct.setImage(updatedProduct.getImage());
         existingProduct.setCategory(updatedProduct.getCategory());
         existingProduct.setDeal(updatedProduct.getDeal());
 
